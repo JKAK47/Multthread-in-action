ae4cb349d6b8aa16e36c9f8d77185e9dec26c304
PR <1557862201@qq.com>
2018-07-10 08:05:58 +0800

    线程安全类注解

---
+++ src/main/java/org/vincent/multthread/Sequence.java
@@ -0,0 +1,17 @@
+package org.vincent.multthread;
+
+/**
+ * @Package: org.vincent.multthread <br/>
+ * @Description： TODO <br/>
+ * @author: PengRong <br/>
+ * @Date: Created in 2018/7/10 8:04 <br/>
+ * @Company: PLCC <br/>
+ * @Copyright: Copyright (c) 2018 <br/>
+ * @Version: 1.0 <br/>
+ * @Modified By: <br/>
+ * @Created by PengRong on 2018/7/10. <br/>
+ */
+
+public class Sequence {
+
+}
