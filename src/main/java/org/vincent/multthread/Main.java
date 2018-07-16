d4e0f55cf1457be45a94b66a3e7a9832463bbf93
PR <1557862201@qq.com>
2018-07-16 08:18:52 +0800

    pom plugin

---
+++ src/main/java/org/vincent/multthread/Main.java
@@ -0,0 +1,29 @@
+package org.vincent.multthread;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @Package: PACKAGE_NAME <br/>
+ * @Description： TODO <br/>
+ * @author: PengRong <br/>
+ * @Date: Created in 2018/7/14 17:03 <br/>
+ * @Company: PLCC <br/>
+ * @Copyright: Copyright (c) 2018 <br/>
+ * @Version: 1.0 <br/>
+ * @Modified By: <br/>
+ * @Created by PengRong on 2018/7/14. <br/>
+ */
+
+public class Main {
+		public static void main(String[] args) throws IOException {
+				System.out.println("hello , welcome to multthread project.");
+				OkHttpClient client = new OkHttpClient();
+				Request request=new  Request.Builder().url("https://www.sogou.com/").build();
+				Response response=client.newCall(request).execute();
+				System.out.println(response.body().toString());
+		    System.out.println("hello ,  bye .");
+		}
+}
---
+++ src/test/java/org/vincent/multthread/TestDemo.java
@@ -0,0 +1,22 @@
+package  org.vincent.multthread;
+
+import org.junit.Test;
+
+/**
+ * @Package: PACKAGE_NAME <br/>
+ * @Description： TODO <br/>
+ * @author: PengRong <br/>
+ * @Date: Created in 2018/7/15 12:52 <br/>
+ * @Company: PLCC <br/>
+ * @Copyright: Copyright (c) 2018 <br/>
+ * @Version: 1.0 <br/>
+ * @Modified By: <br/>
+ * @Created by PengRong on 2018/7/15. <br/>
+ */
+
+public class TestDemo {
+		@Test
+		public void  testDEMO(){
+				System.out.println("test demo ...");
+		}
+}
